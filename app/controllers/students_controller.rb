b6704f26c72f0ca010f76dd5caebd649637671b5
qkid1412 <qingyunq@sfu.ca>
2016-06-05 19:36:45 +0000

    check

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,44 @@
+class StudentsController < ApplicationController
+    
+    def index
+        @std = Student.all
+    end
+    
+    def show
+        @std = Student.find(params[:id])
+    end
+    
+    def edit
+        @std = Student.find(param[:id])
+    end
+    
+    def new
+        @std = Student.new
+    end
+    
+    def create
+        @std = Student.new(student_params)   
+        if @std.save
+            redirect_to @std, notice: 'Student record was successfully created'
+        else
+            render :action => "new"
+        end
+    end
+    
+    def update
+        if @std.update(student_params)
+            redirect_to @std, notice: 'Student record was successfully updated'
+        else
+            render :edit
+        end
+    end
+    def destroy
+        @std = Student.find(params[:id])
+        @std.destroy
+        redirect_to student_url, notice:'Record deleted'
+    end
+    private
+    def student_params
+        params.require(:student).permit(:name, :weight, :height, :color, :gpa, :email)
+    end
+end
---
+++ db/migrate/20160605051331_create_students.rb
@@ -0,0 +1,14 @@
+class CreateStudents < ActiveRecord::Migration
+  def change
+    create_table :students do |t|
+      t.string :name
+      t.integer :weight
+      t.integer :height
+      t.string :color
+      t.decimal :gpa
+      t.string :email
+
+      t.timestamps null: false
+    end
+  end
+end
