c55b5714d12b8ad04c774c4a177d35a7acc9156d
qkid1412 <qingyunq@sfu.ca>
2016-06-06 03:00:09 +0000

    onlyhomepageleft

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,5 @@
 class Student < ActiveRecord::Base
+    validates :height, numericality: { only_integer: true }
+    validates :weight, numericality: { only_integer: true }
+    validates :name, :height, :weight, :color, :gpa, :email, presence: true
 end
