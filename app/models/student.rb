8757d7b2594ee74b0dc4173ef975a013b6def236
qkid1412 <qingyunq@sfu.ca>
2016-06-06 04:45:36 +0000

    almost done

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,6 @@
 class Student < ActiveRecord::Base
-    validates :height, numericality: { only_integer: true }
-    validates :weight, numericality: { only_integer: true }
+    validates :height, numericality: { only_integer: true, message: "Please provide a valid number" }
+    validates :weight, numericality: { only_integer: true, message: "Please provide a valid number" }
     validates :name, :height, :weight, :color, :gpa, :email, presence: true
+    validates :email, length: { minimum: 6, message: "email is too short"}
 end
